de06db27650bfc48c116b52eea6f5850fdea9677
luizgontijo <lz.gontijo@gmail.com>
2020-06-06 17:48:35 -0300

    Add files via upload

---
+++ public/scripts/intro.js
@@ -0,0 +1,83 @@
+//document.write('Hello')
+//
+
+
+// variáveis e tipos de dados
+//var myvar = "Hello"
+
+//document.write(myvar)
+//document.write(myvar)
+//document.write(myvar)
+//document.write(myvar)
+
+//const myconst = "he"
+
+//document.write(myconst+myvar)
+
+//string
+//const s1 = "isso é uma string"
+//const s2 = 'isso é uma string'
+
+//document.write(s2)
+
+//number
+//const n1 = '9'
+//const n2 = 12
+
+//document.write(n1+n2)
+
+//boolean - true or false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+
+//ESTRUTURA DE DADOS
+//objetos possuem propriedades e funcionalidades
+
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//    document.write("executar lógica")
+//  }
+//}
+
+//pessoa.correr()
+
+// Array - vetor
+//const colecaoDeBolinhas = ["blue", "green", 1, true, {name:"Luiz"}]
+
+//document.write(colecaoDeBolinhas[4].name)
+
+
+//Funções - funcionalidades - atalhos - reuso de código
+//function sayMyName(name) {
+  
+//  document.write(name)
+//}
+
+//sayMyName("Lula")
+//sayMyName("Luiz")
+
+
+//Conficionais
+
+//const notaFinal = 5
+
+//if( notaFinal < 5 ) {
+//  document.write("reprovado")
+  
+//} else {
+//    document.write("aprovado")
+  
+//}
+
+
+//Loop - repetições
+
+//for(i = 0; i<10; i++) {
+  //  document.write(`<p> ${i} </>`)
+  //}
+  
